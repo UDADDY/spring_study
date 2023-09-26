6acd155f8e17d26bfcaf42e35aa0618209aa0c4b
UDADDY <gustmd5715@gmail.com>
2023-09-26 14:20:11 +0900

    feat: add ClubMember, ClubMemberRole, ClubMemberRepository

--- club/src/main/java/com/example/club/config/SecurityConfig.java
+++ club/src/main/java/com/example/club/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.club.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,4 +17,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers("/sample/all").permitAll()
+                .antMatchers("/sample/member").hasRole("USER");
+
+        http.formLogin();
+        http.csrf().disable();
+        http.logout();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        // 사용자 계정은 user1
+        auth.inMemoryAuthentication().withUser("user1")
+                .password("$2a$10$2.NC3zTbqt/SBmDS/Uc3xeL0dHCuH062g6xuD/CtOi0HCX.A2sEnO")
+                .roles("USER");
+    }
+
 }
