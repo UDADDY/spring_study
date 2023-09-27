655f9bcb7b3ce7bb2d60e74d3d144c3b63b1a718
UDADDY <gustmd5715@gmail.com>
2023-09-27 15:33:21 +0900

    UserDetails 사용

--- club/src/main/java/com/example/club/security/dto/ClubAuthMemberDTO.java
+++ club/src/main/java/com/example/club/security/dto/ClubAuthMemberDTO.java
@@ -1,5 +1,8 @@
 package com.example.club.security.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -7,6 +10,9 @@ import org.springframework.security.core.userdetails.User;
 import java.util.Collection;
 
 @Log4j2
+@Getter
+@Setter
+@ToString
 public class ClubAuthMemberDTO extends User {
 
     private String email;
--- club/src/main/java/com/example/club/security/service/ClubUserDetailsService.java
+++ club/src/main/java/com/example/club/security/service/ClubUserDetailsService.java
@@ -1,19 +1,52 @@
 package com.example.club.security.service;
 
+import com.example.club.entity.ClubMember;
+import com.example.club.repository.ClubMemberRepository;
+import com.example.club.security.dto.ClubAuthMemberDTO;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 @Log4j2
+@RequiredArgsConstructor
 public class ClubUserDetailsService implements UserDetailsService {
+
+    private final ClubMemberRepository clubMemberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("ClubUserDetailsService loadUserByUserName : " + username);
 
-        return null;
+        Optional<ClubMember> result = clubMemberRepository.findByEmail(username, false);
+
+        if (!result.isPresent()) {
+            throw new UsernameNotFoundException("Check Email or Social");
+        }
+
+        ClubMember clubMember = result.get();
+
+        log.info("---------------");
+        log.info(clubMember);
+
+        ClubAuthMemberDTO clubAuthMember = new ClubAuthMemberDTO(
+                clubMember.getName(),
+                clubMember.getPassword(),
+                clubMember.isFromSocial(),
+                clubMember.getRoleSet().stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name())).collect(Collectors.toList())
+        );
+
+        clubAuthMember.setName(clubMember.getName());
+        clubAuthMember.setFromSocial(clubMember.isFromSocial());
+
+        return clubAuthMember;
     }
 }
